7013fc8a0625f9314bc4fc8043bf25de3992ffa1
LBMSOFTWARES <softwareslbm@gmail.com>
2026-03-17 17:30:02 -0300

    Update settings.py

--- barbeariasite/settings.py
+++ barbeariasite/settings.py
@@ -126,3 +126,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'softwareslbm@gmail.com'
+EMAIL_HOST_PASSWORD = 'vqzj kwix aqrc knsh'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
